4910c4affcbc06d622b48e1e7f95b213681e78b3
pstalteri2I <pstalteri@2innovation.it>
2025-05-22 10:16:31 +0200

    aggiunta delete

---
+++ lib/handlers/api/deletePokemon.dart
@@ -0,0 +1,40 @@
+import 'package:aws_lambda_dart_runtime/aws_lambda_dart_runtime.dart';
+import 'package:aws_lambda_dart_runtime/runtime/context.dart';
+import 'package:aws_client/dynamo_db_2012_08_10.dart';
+import 'package:dart_template/marshal.dart';
+import 'package:dart_template/models/pokemon.dart';
+import 'package:dart_template/unmarshal.dart';
+
+Future<AwsApiGatewayResponse> getPokemon(
+  Context context,
+  AwsApiGatewayEvent event,
+) async {
+  try {
+    final db = DynamoDB(region: context.region!);
+
+    final pokemonID = event.pathParameters?['pokemonID'];
+
+    final results = await db.deleteItem(
+      key: marshall({"pokemonID": pokemonID}),
+      tableName: "pokemons",
+      returnValues: ReturnValue.allOld,
+    );
+
+    if (results.attributes == null) {
+      return AwsApiGatewayResponse.fromJson({
+        'status': 'error',
+        'content': 'Pokemon not found',
+      });
+    }
+    final deletedPokemon = Pokemon.fromJson(unmarshal(results.attributes!));
+
+    return AwsApiGatewayResponse.fromJson(
+        {'status': 'ok', 'content': deletedPokemon.toJson()});
+  } catch (e) {
+    return AwsApiGatewayResponse.fromJson({
+      'status': 'error',
+      'content': 'Error creating Pokemon',
+      'error': e.toString(),
+    });
+  }
+}
