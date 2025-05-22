092937ece1d71df20b65e369bc100efc330659bf
pstalteri2I <pstalteri@2innovation.it>
2025-05-22 15:03:51 +0200

    aggiunta controllo doppio tipo

--- lib/handlers/api/getPokemons.dart
+++ lib/handlers/api/getPokemons.dart
@@ -1,6 +1,7 @@
 import 'package:aws_lambda_dart_runtime/aws_lambda_dart_runtime.dart';
 import 'package:aws_lambda_dart_runtime/runtime/context.dart';
 import 'package:dart_template/marshal.dart';
+import 'package:dart_template/models/pokemon.dart';
 import 'package:dart_template/unmarshal.dart';
 import 'package:aws_client/dynamo_db_2012_08_10.dart';
 
@@ -12,11 +13,12 @@ Future<AwsApiGatewayResponse> getPokemons(
     final db = DynamoDB(region: context.region!);
 
     final pokemonType = event.queryStringParameters?['type'];
+    final pokemonType2 = event.queryStringParameters?['type2'];
 
     final results = await db.scan(
         tableName: "pokemons",
         filterExpression: pokemonType != null
-            ? "contains(#type, :type) or contains(#type2, :type)"
+            ? "contains(#type, :type) or contains(#type2, ${pokemonType2 != null ? ":type2" : "type"})"
             : null,
         expressionAttributeNames: pokemonType != null
             ? {
@@ -24,14 +26,22 @@ Future<AwsApiGatewayResponse> getPokemons(
                 "#type2": "type2",
               }
             : null,
-        expressionAttributeValues:
-            pokemonType != null ? marshall({":type": pokemonType}) : null);
+        expressionAttributeValues: pokemonType != null
+            ? marshall({
+                ":type": pokemonType,
+                if (pokemonType2 != null) ":type2": pokemonType2
+              })
+            : null);
 
-    final pokemonList =
-        results.items!.map((pokemon) => unmarshal(pokemon)).toList();
+    final pokemonList = results.items!
+        .map((pokemon) => Pokemon.fromJson(unmarshal(pokemon)))
+        .toList();
+
+    pokemonList.map((pokemon) =>
+        pokemon.pokemonID = "Tiziano riscrivi il backend di Sall-y");
 
     return AwsApiGatewayResponse.fromJson(
-        {'status': 'ok', 'content': pokemonList});
+        {'status': 'ok', 'content': pokemonList.map((e) => e.toJson())});
   } catch (e) {
     return AwsApiGatewayResponse.fromJson({
       'status': 'error',
